169c664a1037278a15a817d7a7b534093c403972
John Galyon <galyon.jb@gmail.com>
2020-11-02 17:10:27 -0500

    updates to wpgulp

--- wpgulp.config.js
+++ wpgulp.config.js
@@ -12,7 +12,7 @@
 // Local project URL of your already running WordPress site.
 // > Could be something like "wpgulp.local" or "localhost"
 // > depending upon your local WordPress setup.
-const projectURL = 'wpgulp.local';
+const projectURL = '127.0.0.1:8080';
 
 // Theme/Plugin URL. Leave it like it is; since our gulpfile.js lives in the root folder.
 const productURL = './';
@@ -21,10 +21,10 @@ const injectChanges = true;
 
 // >>>>> Style options.
 // Path to main .scss file.
-const styleSRC = './assets/css/style.scss';
+const styleSRC = './dev/css/style.scss';
 
 // Path to place the compiled CSS file. Default set to root folder.
-const styleDestination = './';
+const styleDestination = './build/assets';
 
 // Available options → 'compact' or 'compressed' or 'nested' or 'expanded'
 const outputStyle = 'compact';
@@ -34,10 +34,10 @@ const precision = 10;
 // JS Vendor options.
 
 // Path to JS vendor folder.
-const jsVendorSRC = './assets/js/vendor/*.js';
+const jsVendorSRC = './dev/js/vendor/*.js';
 
 // Path to place the compiled JS vendors file.
-const jsVendorDestination = './assets/js/';
+const jsVendorDestination = './build/assets/';
 
 // Compiled JS vendors file name. Default set to vendors i.e. vendors.js.
 const jsVendorFile = 'vendor';
@@ -45,10 +45,10 @@ const jsVendorFile = 'vendor';
 // JS Custom options.
 
 // Path to JS custom scripts folder.
-const jsCustomSRC = './assets/js/custom/*.js';
+const jsCustomSRC = './dev/js/custom/*.js';
 
 // Path to place the compiled JS custom scripts file.
-const jsCustomDestination = './assets/js/';
+const jsCustomDestination = './build/assets/';
 
 // Compiled JS custom file name. Default set to custom i.e. custom.js.
 const jsCustomFile = 'custom';
@@ -57,31 +57,31 @@ const jsCustomFile = 'custom';
 
 // Source folder of images which should be optimized and watched.
 // > You can also specify types e.g. raw/**.{png,jpg,gif} in the glob.
-const imgSRC = './assets/img/raw/**/*';
+const imgSRC = './dev/img/raw/**/*';
 
 // Destination folder of optimized images.
 // > Must be different from the imagesSRC folder.
-const imgDST = './assets/img/';
+const imgDST = './build/assets/img/';
 
 // >>>>> Watch files paths.
 // Path to all *.scss files inside css folder and inside them.
-const watchStyles = './assets/css/**/*.scss';
+const watchStyles = './dev/css/**/*.scss';
 
 // Path to all vendor JS files.
-const watchJsVendor = './assets/js/vendor/*.js';
+const watchJsVendor = './dev/js/vendor/*.js';
 
 // Path to all custom JS files.
-const watchJsCustom = './assets/js/custom/*.js';
+const watchJsCustom = './dev/js/custom/*.js';
 
 // Path to all PHP files.
 const watchPhp = './**/*.php';
 
 // >>>>> Zip file config.
 // Must have.zip at the end.
-const zipName = 'theme.zip';
+const zipName = 'coefoodsafetytools.org.zip';
 
 // Must be a folder outside of the zip folder.
-const zipDestination = './../'; // Default: Parent folder.
+const zipDestination = '.'; // Default: Parent folder.
 const zipIncludeGlob = ['./**/*']; // Default: Include all files/folders in current directory.
 
 // Default ignored files and folders for the zip file.
@@ -122,13 +122,13 @@ const translationDestination = './languages';
 const packageName = 'WPGULP';
 
 // Where can users report bugs.
-const bugReport = 'https://AhmadAwais.com/contact/';
+const bugReport = 'https://github.com/galyonj/coe-theme/issues/;
 
 // Last translator Email ID.
-const lastTranslator = 'Ahmad Awais <your_email@email.com>';
+const lastTranslator = 'John Galyon <galyon.jb@email.com>';
 
 // Team's Email ID.
-const team = 'AhmadAwais <your_email@email.com>';
+const team = 'John Galyon <galyon.jb@email.com>';
 
 // Browsers you care about for auto-prefixing. Browserlist https://github.com/ai/browserslist
 // The following list is set as per WordPress requirements. Though; Feel free to change.
